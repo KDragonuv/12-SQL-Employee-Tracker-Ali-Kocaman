0d6ce6b431742afb496d14cddbc86ed690730868
Ali K <aliekx609@gmail.com>
2023-12-21 23:00:56 +0000

    table sql

---
+++ db/schema.sql
@@ -0,0 +1,34 @@
+DROP DATABASE employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
+
+USE employee_tracker_db;
+--comments 
+CREATE TABLE department ( 
+
+    id INT AUTO_INCREMENT NOT NULL, 
+    name VARCHAR(30)
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE roles (
+    id INT AUTO_INCREMENT NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    PRIMARY KEY (id)
+    FOREIGN KEY (department_id)
+    REFERENCES department (id)
+);
+
+CREATE TABLE employee (
+    id INT AUTO_INCREMENT NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id)
+    FOREIGN KEY (role_id)
+    REFERENCES roles (id)
+);
+
