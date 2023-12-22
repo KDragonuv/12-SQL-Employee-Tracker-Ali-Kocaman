dc93625fafd86533e872126ec9bff6e40dbe06cc
Ali K <aliekx609@gmail.com>
2023-12-22 00:46:37 +0000

    adding missing comments and a few functions

--- db/db.js
+++ db/db.js
@@ -46,9 +46,69 @@ async function initializeDatabase() {
   }
 
   async function addDepartment() {
-    const departmentName = await inquirer.prompt({ type: 'input', name: 'name', message: 'Enter the name of the department:' });
+    const departmentName = await inquirer.prompt({ type: 'input', name: 'name', message: 'Enter the name of the department:' });   // <= Prompt user for information.
     await executeQuery(addDepartmentQuery, [departmentName.name]);
     console.log('Department added successfully!');
   }
 
-  module.exports = { initializeDatabase, viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole };
+  async function addRole() {
+    try {
+        const roleInfo = await inquirer.prompt([     // <= Prompt user for role information.
+        { type: 'input', name: 'title', message: 'Enter the title of the role:' },
+        { type: 'input', name: 'salary', message: 'Enter the salary for the role:' },
+        { type: 'input', name: 'departmentId', message: 'Enter the department ID for the role:' },
+      ]);
+  
+      await executeQuery(addRoleQuery, [roleInfo.title, roleInfo.salary, roleInfo.departmentId]);     // <= Execute the addRoleQuery with what information, the user provided. 
+  
+      console.log('Role added successfully!');
+    } catch (error) {
+      console.error('Error adding role:', error);
+    }
+  }
+
+  async function addEmployee() {
+    try {
+        const employeeInfo = await inquirer.prompt([     // <= Prompt user for employee information.
+        { type: 'input', name: 'firstName', message: 'Enter the first name of the employee:' },
+        { type: 'input', name: 'lastName', message: 'Enter the last name of the employee:' },
+        { type: 'input', name: 'roleId', message: 'Enter the role ID for the employee:' },
+        { type: 'input', name: 'managerId', message: 'Enter the manager ID for the employee (optional):' },
+      ]);
+  
+      await executeQuery(addEmployeeQuery, [       // <= Execute the addEmployeeQuery with user-provided information.
+        employeeInfo.firstName,
+        employeeInfo.lastName,
+        employeeInfo.roleId,
+        employeeInfo.managerId || null,     // <= If managerId is not provided, set it to null.
+      ]);
+  
+      console.log('Employee added successfully!');
+    } catch (error) {
+      console.error('Error adding employee:', error);
+    }
+  }
+  
+  async function updateEmployeeRole() {
+    try {
+
+      const employees = await executeQuery('SELECT employee_id, CONCAT(first_name, " ", last_name) AS employee_name FROM employees');      // <= Get a list of employees for the user to choose from.
+  
+      const selectedEmployee = await inquirer.prompt({       // <= Prompt user to select an employee to update.
+        type: 'list',
+        name: 'employeeId',
+        message: 'Select an employee to update their role:',
+        choices: employees.map((employee) => ({ name: employee.employee_name, value: employee.employee_id })),
+      });
+  
+      const newRoleId = await inquirer.prompt({ type: 'input', name: 'newRoleId', message: 'Enter the new role ID for the employee:' });    // <= Prompt user for the new role ID.
+  
+      await executeQuery(updateEmployeeRoleQuery, [newRoleId.newRoleId, selectedEmployee.employeeId]);   // <= Execute the updateEmployeeRoleQuery with user-provided information.
+  
+    console.log('Employee role updated successfully!');
+    } catch (error) {
+      console.error('Error updating employee role:', error);      
+    }
+  }
+
+    module.exports = { initializeDatabase, viewDepartments, viewRoles, viewEmployees, addDepartment, addRole, addEmployee, updateEmployeeRole };
